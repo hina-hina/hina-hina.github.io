f41845fc5c35d571d5d8b23ba47eac0222885d60
hina-hina <68209524+hina-hina@users.noreply.github.com>
2020-07-20 18:30:37 +0900

    ポートフォリオ

---
+++ port.js
@@ -0,0 +1,12 @@
+function setcolor(obj0,sw1) {
+    if (sw1 == 0) {
+      fgc = '';
+      bgc = '';
+    } else {
+      fgc = '#FF0461';
+      bgc = '#003399';
+    }
+    obj0.style.color = fgc;
+    obj0.style.backgroundColor = bgc;
+  }
+  
